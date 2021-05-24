988add40ae1c8e26a973d066924e0c242df0dc05
Joshua Higgins <higginsjoshuat@gmail.com>
2021-05-24 11:33:14 -0400

    Added the frame for the final output message

--- mixedMessages.js
+++ mixedMessages.js
@@ -32,15 +32,9 @@ const message = {
     },
     generateAction(){
         return;
+    },
+    createMessage(){
+        return;
     }
+    
 }
-
-
-const generateGod = () => {
-    // Generate a Random Number
-    let randomNumber = Math.floor(Math.random() * 6);
-
-    console.log(randomNumber); 
-}
-
-generateGod()
