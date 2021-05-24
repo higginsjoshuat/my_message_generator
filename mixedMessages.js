f7ce4d3cc6cf4de9c2ae5166c8ddfde7bae614c8
Joshua Higgins <higginsjoshuat@gmail.com>
2021-05-24 11:27:03 -0400

    Set up message object, created framework for arrays, getters, and methods. Added Comments.

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,3 +1,39 @@
+// Mixed Messages Program for Codecademy
+
+// Creates our message object.
+const message = {
+    // Creating arrays.
+    _gods: [],
+    _emotions: [],
+    _elements: [],
+    _actions:[],
+    // Creating Getter Methods
+    get gods() {
+        return this._gods;
+    },
+    get elements(){
+        return this._elements;
+    },
+    get actions(){
+        return this._actions;
+    },
+    get emotions(){
+        return this._emotions;
+    },
+    // Creating methods for generating pieces of the message.
+    generateGod(){
+        return; 
+    },
+    generateEmotion(){
+        return;
+    },
+    generateElement(){
+        return;
+    },
+    generateAction(){
+        return;
+    }
+}
 
 
 const generateGod = () => {
