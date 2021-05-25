7a388e72c9682521682bbf52df807d835f5b91c6
Joshua Higgins <higginsjoshuat@gmail.com>
2021-05-25 07:49:19 -0400

    Fixed the random generation for emotion and element. Made the functionality for generating the action. Completed the message generation function. ready for testing.

--- mixedMessages.js
+++ mixedMessages.js
@@ -10,19 +10,19 @@ const message = {
         // angry
         ['sends destructive energy your way!', 'demands blood!', 'says you will pay dearly in the coming days.'],
         // loving
-        ['','',''],
+        ['says your true love is closer than you think.','says a new life will make it\'s way into yours.','fills you with warmth <3.'],
         // frisky
-        ['','',''],
+        ['will visit soon for a midnight treat.','pushes all the blood in all the right places.','will soon make you irresistible.'],
         // drunk
-        ['','',''],
+        ['will fill your cup eternally!','sees a blurry vision of your future.','has fallen asleep at the wheel.'],
         // spiteful
-        ['','',''],
+        ['moves your furniture ever-so slightly left.','will decline bank information for the next 24 hours.','sends a horde of acne to that beautiful skin tone.'],
         // devious
-        ['','',''],
+        ['plots a week full of cunning pranks.','makes you feel more forgetful than usual.','pokes holes in your lambskin.'],
         // war-bound
-        ['','',''],
+        ['calls you to fight along the path to victory!','will push you to complete your tasks with a vengeance!','sends misfortune to your enemies!'],
         // hyper
-        ['','','']
+        ['warn\'s you that coffee has some... side effects.','curses you with everlasting pacing habits.','invites you to go streaking.. could be fun!']
     ],
     // Creating Getter Methods
     get gods() {
@@ -43,18 +43,52 @@ const message = {
         return this.gods[randomNum]; 
     },
     generateEmotion(){
-        let randomNum = Math.floor(Math.random() * this.gods.length);
+        let randomNum = Math.floor(Math.random() * this.emotions.length);
         return this.emotions[randomNum];
     },
     generateElement(){
-        let randomNum = Math.floor(Math.random() * this.gods.length);
+        let randomNum = Math.floor(Math.random() * this.elements.length);
         return this.elements[randomNum];
     },
-    generateAction(){
-        return;
+    generateAction(emotion){
+        let randomNum = Math.floor(Math.random() * 3);
+        let act = 0;
+        switch (emotion) {
+            case 'angry':
+                act = 0
+                break;
+            case 'loving':
+                act = 1
+                break;
+            case 'frisky':
+                act = 2
+                break;
+            case 'drunk':
+                act = 3
+                break;
+            case 'spiteful':
+                act = 4
+                break;
+            case 'devious':
+                act = 5
+                break;
+            case 'war-bound':
+                act = 6
+                break;
+            case 'hyper':
+                act = 7
+                break;
+            default:
+                return "Error: Something went wrong in generating an action!";
+    }
+        return this._actions[act][randomNum];
     },
     createMessage(){
-        return;
+        const god = generateGod();
+        const element = generateElement();
+        const emotion = generateEmotion();
+        const action = generateAction(emotion);
+        return `${god} is feeling ${emotion}. ${god} conjures ${element} and ${action}`;
     }
     
 }
